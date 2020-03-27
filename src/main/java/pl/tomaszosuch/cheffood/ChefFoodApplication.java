07058b9b5d9bb26e817d359f187860d0606a6315
tomOsuch <tomasz.osuch@gmail.com>
2020-03-27 21:54:58 +0100

    Initial commit

---
+++ src/main/java/pl/tomaszosuch/cheffood/ChefFoodApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszosuch.cheffood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChefFoodApplication.class, args);
+    }
+
+}
